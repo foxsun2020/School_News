ec417f1393da35586ed3a09fca4e706b3f616bde
foxsun2020 <yosen7@gmail.com>
2020-07-12 22:40:10 +0800

    add default e-mail service add school news add College of Fashion news

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from re import compile
 
 def send_email(_user, _password, _host, _email, _contents):
     yag = yagmail.SMTP(user=_user, password=_password, host=_host)
-    send_contents = _contents + '\n\n\npowered by fox2020.cn'
+    send_contents = _contents + '\n\n\npowered by https://fox2020.cn'
     yag.send(_email, 'SUES_News_Update', send_contents)
     print('Sent successfully')
     print('from %s to %s' % (_user, _email))
 
 
-def ge_spider():
+def ge_spider():  # graduate school news
     url = 'https://ge.sues.edu.cn/19716/list.htm'
     ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
          '(KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.58'
@@ -28,25 +28,50 @@ def ge_spider():
         link = urljoin(url, s_link)
         date = item.find('span', class_="news_meta").text
         news = title + '\n' + link + '\n' + date
-        ge_news = archive(title, news)
-        return ge_news
+        archive(title, news)
 
 
-def school_spider():
-    url = 'https://www.sues.edu.cn/xxyw/list.htm'
+def fashion_spider():
+    url = 'https://cfd.sues.edu.cn/11394/list.htm'
     ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
          '(KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.58'
     response = get(url, headers={'User-Agent': ua})
     data = response.content.decode('utf-8')
     soup = BeautifulSoup(data, 'html.parser')
-    s = soup.findAll('a', class_=compile(r"column-news-item item-(.?) clearfix"))
+    s = soup.findAll('td', align='left')
     for item in s:
-        title = item.find('span', class_='column-news-title').text
-        link = urljoin(url, item['href'])
-        date = item.find('span', class_='column-news-date news-date-hide').text
-        news = title + '\n' + link + '\n' + date
-        school_news = archive(title, news)
-        return school_news
+        title = '★' + item.find('a', href=compile(r'(\w)'))['title'] + '★'
+        s_link = item.find('a', href=compile(r'(\w)'))['href']
+        link = urljoin(url, s_link)
+        news = title + '\n' + link
+        archive(title, news)
+
+
+def school_spider():  # report news
+    url1 = 'https://www.sues.edu.cn/xsbg/list.htm'
+    url2 = 'https://www.sues.edu.cn/17467/list.htm'
+    url3 = 'https://www.sues.edu.cn/17466/list.htm'
+    url4 = 'https://www.sues.edu.cn/17465/list.htm'
+    url5 = 'https://www.sues.edu.cn/xxyw/list.htm'
+    url6 = 'https://www.sues.edu.cn/xykx/list.htm'
+    url7 = 'https://www.sues.edu.cn/17468/list.htm'
+    url8 = 'https://www.sues.edu.cn/mtjj/list.htm'
+    url9 = 'https://www.sues.edu.cn/17469/list.htm'
+    url10 = 'https://www.sues.edu.cn/82/list.htm'
+    for i in range(1, 11):
+        url = eval('url%s' % i)
+        ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
+             '(KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.58'
+        response = get(url, headers={'User-Agent': ua})
+        data = response.content.decode('utf-8')
+        soup = BeautifulSoup(data, 'html.parser')
+        s = soup.findAll('a', class_=compile(r"column-news-item item-(.?) clearfix"))
+        for item in s:
+            title = item.find('span', class_='column-news-title').text
+            link = urljoin(url, item['href'])
+            date = item.find('span', class_='column-news-date news-date-hide').text
+            news = title + '\n' + link + '\n' + date
+            archive(title, news)
 
 
 def archive(title, news):
@@ -58,24 +83,39 @@ def archive(title, news):
         content = file.read()
         file.seek(0, 0)
         file.write(news + '\n' + '-' * 100 + '\n' + content)
-        if send:
-            send_email(_user, _password, _host, _email, news)
-        return title
+        if send:  # send mail every 10 updates
+            global mail_counter
+            file2 = open(r'send_news.txt', 'r+', encoding='utf-8')
+            file2.seek(0, 0)
+            content = file2.read()
+            file2.seek(0, 0)
+            file2.write(news + '\n\n' + '-' * 100 + '\n\n' + content)
+            mail_counter += 1
+            if mail_counter == 10:
+                file2.seek(0, 0)
+                send_news = file2.read()
+                send_email(_user, _password, _host, _email, send_news)
+                file2.seek(0, 0)
+                file2.truncate()
+            file2.close()
+        print(title)
     file.close()
 
 
 def run(i):
     counter = 0
     while True:
-        counter += 1
+        counter += 0  # run counter
         print('NO.' + str(counter) + '-' * 3 + time.strftime("%m/%d/%Y %H:%M"))
-        s = school_spider()
-        g = ge_spider()
-        if s is not None:
-            print('school news:', s)
-        if g is not None:
-            print('graduate news:', g)
-        if s is None and g is None:
+        f = open(r'News_Archive.txt', 'r', encoding='utf-8')
+        old = f.readline()
+        f.close()
+        school_spider()
+        ge_spider()
+        fashion_spider()
+        f = open(r'News_Archive.txt', 'r', encoding='utf-8')
+        new = f.readline()
+        if old == new:
             print('No Update')
         time.sleep(i)
 
@@ -83,17 +123,21 @@ def run(i):
 if __name__ == '__main__':
     send = False
     update = None
-    need_mail = input('do you send a email?(Y/N)\n')
+    need_mail = input('do you need email notice every 10 updates?(Y/N)\n')
     interval = input('every interval time (unit:s)\n')
     if need_mail == 'y' or need_mail == 'Y':
+        mail_counter = 0
         send = True
-        print('Email Information Setting(*all mail services are supposed)')
+        print('Email Information Setting')
         # you can change the following values with '_' to immutable strings
         # according to your information.
-        _user = input('From>>>(mail address: eg:xxxxxxxxx@sues.edu.cn)\n')
-        _password = input('mail password\n')
-        _host = input('email host(SUES school mail STMP: smtphz.qiye.163.com)\n')
-        _email = input('>>>To(mail address: eg:xxxxxx@gmail.com)\n')
+        # _user = input('From>>>(mail address: eg:xxxxxxxxx@sues.edu.cn)\n')
+        # _password = input('mail password\n')
+        # _host = input('email host(SUES school mail STMP: smtphz.qiye.163.com)\n')
+        _user = 'suesedu@aliyun.com'
+        _password = 'sues2020'
+        _host = 'smtp.aliyun.com'
+        _email = input('>>>To(mail address: eg: recipient@gmail.com)\n')
         print('*' * 5, 'from', _user, '>>>', 'to', _email, '*' * 5)
         run(int(interval))
     else:
